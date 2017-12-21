9428722748aa2e2d5641ef34ad08ef2164ab4dfa
milk448249712 <448249712@qq.com>
2017-12-21 19:11:42 +0800

    milestone

--- location_test/app/src/main/java/com/unnkai/location_test/MainActivity.java
+++ location_test/app/src/main/java/com/unnkai/location_test/MainActivity.java
@@ -3,6 +3,8 @@ package com.unnkai.location_test;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
@@ -45,8 +47,11 @@ import org.json.JSONObject;
 import org.apache.http.entity.StringEntity;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 // import com.google.android.gms.location.LocationServices;
@@ -60,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     private LocationManager lm;
     private static final String TAG = "GpsActivity";
     private int cnt = 0;
+    private long lastGpsTime = 0;
     //private Handler handler=null;
     //在handler中更新UI
     private Handler mHandler = new Handler(){
@@ -67,13 +73,23 @@ public class MainActivity extends AppCompatActivity {
             Location newLoc = (Location)msg.obj;
             //editText.setText("你想变的内容:"+msg.obj);
             editText.setText(cnt+"设备位置信息\n\n经度：");
-            if (newLoc!=null) {
+            if (newLoc!=null && newLoc.getTime()!=lastGpsTime) {
                 editText.append(String.valueOf(newLoc.getLongitude()));
                 editText.append("\n纬度：");
                 editText.append(String.valueOf(newLoc.getLatitude()));
                 editText.append("\nLocal is not null");
+                lastGpsTime = newLoc.getTime();
+                String strAdde = getPositionByGeocoder(newLoc);
+                editText.append("\n\n地址："+strAdde);
             } else {
-                getLocationFromGsm(editText);
+                newLoc = getBestLocation(lm);
+                editText.setText(cnt+"设备位置信息\n\n经度：");
+                editText.append(String.valueOf(newLoc.getLongitude()));
+                editText.append("\n纬度：");
+                editText.append(String.valueOf(newLoc.getLatitude()));
+                editText.append("\ndata from network");
+                String strAdde = getPositionByGeocoder(newLoc);
+                editText.append("\n\n地址："+strAdde);
             }
 
             cnt++;
@@ -98,13 +114,6 @@ public class MainActivity extends AppCompatActivity {
         MyQueryLocationThread myQueryThread = new MyQueryLocationThread();
         myQueryThread.start();
 
-        /*handler=new Handler();
-        new Thread(){
-            public void run(){
-
-                handler.post(runnableUi);
-            }
-        }.start();*/
         // 判断GPS是否正常启动
         if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             Toast.makeText(this, "请开启GPS导航...", Toast.LENGTH_SHORT).show();
@@ -123,7 +132,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "gps permission check failed...", Toast.LENGTH_SHORT).show();
             return;
         }
-        Location location = lm.getLastKnownLocation(bestProvider);
+        // Location location = lm.getLastKnownLocation(bestProvider);
+        Location location = getBestLocation(lm);
         updateView(location);
         // 监听状态
         // lm.addGpsStatusListener(listener);
@@ -137,19 +147,19 @@ public class MainActivity extends AppCompatActivity {
         // 1秒更新一次，或最小位移变化超过1米更新一次；
         // 注意：此处更新准确度非常低，推荐在service里面启动一个Thread，在run中sleep(10000);然后执行handler.sendMessage(),更新位置
 
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3*1000, 0, locationListener);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10*1000, 0, locationListener);
 
     }
 
     private boolean checkLocationFinePermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return false;
         } else {
             return true;
         }
     }
     private boolean checkLocationCoarsePermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return false;
         } else {
             return true;
@@ -165,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onLocationChanged(Location location) {
             Log.i(TAG, "onLocationChanged");
+            Toast.makeText(getApplicationContext(), "on location changed...", Toast.LENGTH_SHORT).show();
             //updateView(location);
             Log.i(TAG, "时间：" + location.getTime());
             Log.i(TAG, "经度：" + location.getLongitude());
@@ -232,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // 清空EditText对象
             editText.getEditableText().clear();
-
+            editText.setText("location is null...");
             /*if (!lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
                 return;
             }
@@ -272,7 +283,29 @@ public class MainActivity extends AppCompatActivity {
         criteria.setPowerRequirement(Criteria.POWER_LOW);
         return criteria;
     }
-
+    private Location getBestLocation(LocationManager locationManager) {
+        Location result = null;
+        if (locationManager != null) {
+            if(!checkLocationFinePermission()) {
+                Toast.makeText(this, "gps permission check failed...", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+            result = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (result != null) {
+                //Toast.makeText(this, "return gps location", Toast.LENGTH_SHORT).show();
+                return result;
+            } else {
+                if(!checkLocationCoarsePermission()) {
+                    Toast.makeText(this, "network permission check failed...", Toast.LENGTH_SHORT).show();
+                    return null;
+                }
+                result = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                //Toast.makeText(this, "return network location", Toast.LENGTH_SHORT).show();
+                return result;
+            }
+        }
+        return result;
+    }
     /** Called when the user clicks the Send button */
     /*public void sendMessage(View view) {
         // Do something in response to button
@@ -323,8 +356,7 @@ public class MainActivity extends AppCompatActivity {
      获取经纬度
      @throws Exception
      */
-    private SItude
-    getItude(SCell cell) throws Exception
+    private SItude getItude(SCell cell) throws Exception
     {
         SItude itude = new SItude();
         /** 采用Android默认的HttpClient */
@@ -378,16 +410,13 @@ public class MainActivity extends AppCompatActivity {
         return itude;
     }
 
-    /**
-     * 获取地理位置
-     @throws Exception
-     */
-    private String
-    getLocation(SItude itude) throws Exception
+    /*** 获取地理位置@throws Exception*/
+    private String getLocation(SItude itude) throws Exception
     {
         String resultString = "";
         /** 这里采用get方法，直接将参数加到URL上 */
-        String urlString = String.format("http://maps.google.cn/maps/geo?key=abcdefg&q=%s,%s", itude.latitude, itude.longitude);
+        // String urlString = String.format("http://maps.google.cn/maps/geo?key=abcdefg&q=%s,%s", itude.latitude, itude.longitude);
+        String urlString = String.format("http://maps.google.cn/maps/api/geocode/json?latlng=%s,%s&sensor=true&language=zh-CN",itude.latitude, itude.longitude);
         Log.i("URL", urlString);
         /** 新建HttpClient */
         HttpClient client = new DefaultHttpClient();
@@ -406,8 +435,7 @@ public class MainActivity extends AppCompatActivity {
             }
             resultString = strBuff.toString();
             /** 解析JSON数据，获得物理地址 */
-            if (resultString != null &&
-                    resultString.length() > 0)
+            if (resultString != null && resultString.length() > 0)
             {
                 JSONObject jsonobject = new JSONObject(resultString);
                 JSONArray jsonArray = new JSONArray(jsonobject.get("Placemark").toString());
@@ -425,6 +453,56 @@ public class MainActivity extends AppCompatActivity {
             client = null;
         }
         return resultString;
+    }
+    private String getPositionByGeocoder(Location location) {
+        String strPositon = "";
+        String errorMessage = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            errorMessage = "service_not_available";
+            Log.e(TAG, errorMessage, ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            errorMessage = "invalid_lat_long_used";
+            Log.e(TAG, errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = "no_address_found";
+                Log.e(TAG, errorMessage);
+            }
+            return "";
+        } else {
+            Address address = addresses.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+
+            // Fetch the address lines using getAddressLine,
+            // join them, and send them to the thread.
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+            Log.i(TAG, "address_found");
+            if(addressFragments.size() > 0) {
+                strPositon = addressFragments.get(0);
+            }
+            /*int add_cnt = 0;
+            for(String addr : addressFragments) {
+                Toast.makeText(this,"geo pos "+add_cnt+":"+addr,Toast.LENGTH_SHORT).show();
+            }*/
+        }
+        return strPositon;
     }
     /** 显示结果 */
     private void showResult(SCell cell, String location,EditText cellText) {
@@ -471,6 +549,11 @@ public class MainActivity extends AppCompatActivity {
         //继承Thread类，并改写其run方法
         private final static String TAG = "My Thread ===> ";
         public void run(){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             Log.d(TAG, "run");
             for(int i = 0; i<10000; i++)
             {
