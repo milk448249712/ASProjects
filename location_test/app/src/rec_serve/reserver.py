44644a11fb9b6fed383e33acaaaaf0a770b6184e
milk448249712 <448249712@qq.com>
2019-08-04 23:39:37 +0800

    add recive server

---
+++ location_test/app/src/rec_serve/reserver.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import os
+import sqlite3
+
+from tornado.options import define, options
+ 
+print("你好，世界")
+define("port", default=9998, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    global MOBILE_STARTUP_NODES
+    def get(self):   
+        device_id = self.get_argument('device_id', 'no device id')
+        id_type = self.get_argument('id_type', 'no id type')
+        self.write('<p>device_id:['+device_id+']</p>')
+        self.write('<p>id_type:['+id_type+']</p>')
+        conn = sqlite3.connect('locationManage.db')
+        c = conn.cursor()
+        cursor = c.execute('''
+            SELECT count(*) FROM sqlite_master
+            WHERE type='table' AND
+            name='location';''')
+        print("cursor:",cursor)
+        for row in cursor:
+            print("count:",row[0])
+            if row[0] == 1 :
+                print("table is exists.")
+                break
+            else :
+                c.execute('''CREATE TABLE location
+                    (ID INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
+                    lat           REAL    NOT NULL,
+                    long            REAL     NOT NULL,
+                    target        TEXT,
+                    time         INTEGER);''')
+                conn.commit()
+                print("Table created successfully")
+                break
+        c.execute("INSERT INTO location (ID,lat,long,target,time) \
+            VALUES (NULL, 123.34, 567.89, '"+device_id+"', "+id_type+" )");
+        conn.commit()
+        cursor = c.execute("SELECT ID, lat, long, target, time  from location")
+        for row in cursor:
+           self.write("<p>ID = "+str(row[0]))
+           self.write("lat = "+str(row[1]))
+           self.write("long = "+str(row[2]))
+           self.write("target = "+row[3])
+           self.write("time = "+str(row[4])+"</p>")
+        conn.close()
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([(r"/location",IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
