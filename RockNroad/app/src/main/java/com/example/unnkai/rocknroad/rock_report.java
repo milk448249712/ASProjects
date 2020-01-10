339251dbb141e586038062143a7bd71adc5aa84f
chenyunhai <chenyunhai@zuoyebang.com>
2020-01-10 20:30:07 +0800

    upload from thinkpad

--- RockNroad/app/src/main/java/com/example/unnkai/rocknroad/rock_report.java
+++ RockNroad/app/src/main/java/com/example/unnkai/rocknroad/rock_report.java
@@ -15,17 +15,17 @@ public class rock_report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock_report);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
